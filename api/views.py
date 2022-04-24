d7e7f42c5129f8b0493a53b81a42b9b4edadb639
Jay Kothari <jaykothari49@gmail.com>
2022-04-24 23:01:07 +0530

    data Formated

--- api/views.py
+++ api/views.py
@@ -24,18 +24,26 @@ def get_data(request):
     if request.method == 'POST':
         print(request.data,'------------------------------------------')
         symbol = request.data['symbol']
-        start = request.data['start'].split('-')
-        end = request.data['end'].split('-')
+        # start = request.data['start'].split('-')
+        # end = request.data['end'].split('-')
+        # sbin = get_history(symbol=symbol,
+        #             start=date(int(start[0]),int(start[1]),int(start[2])),
+        #             end=date(int(end[0]),int(end[1]),int(end[2])))
+
         sbin = get_history(symbol=symbol,
-                    start=date(int(start[0]),int(start[1]),int(start[2])),
-                    end=date(int(end[0]),int(end[1]),int(end[2])))
+                    start=date(2022,3,1),
+                    end=date.today())[::-1]
         d = dict(sbin)
-        data = {}
-        l = ['Series','Open','High','Low','Close']
+        data = {'Info': {'Symbol' : symbol}, 'Time' : {}}
+        for i in range(7):
+            try:
+                data['Info']['Series'] = d['Series'][date(2022,3,i)]
+            except:pass
+        l = ['Open','High','Low','Close']
         for k,v in d['Symbol'].items():
-            data.update({str(k):{'symbol':v}})
+            data['Time'].update({str(k):{}})
         for i in l:
             for k,v in d[i].items():
-                data[str(k)].update({i.lower():v})
-        return JsonResponse(data)
+                data['Time'][str(k)].update({i.lower():v})
+        return Response(data)
     return Response('GET Method')
